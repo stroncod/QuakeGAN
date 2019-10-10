ae8b781dd24fa0b0e3d3f3022cbc03e149652245
stroncod <stroncod@gmail.com>
2019-10-10 20:51:56 -0300

    added some results

--- photos.py
+++ photos.py
@@ -8,15 +8,24 @@ from mpl_toolkits.basemap import Basemap
 import os
 from PIL import ImageChops
 import glob
+import tqdm
 mpl.use('Agg')
 plt.ioff()
 
 TIME_STEP = 9
 IMG_SIZE = 32
-TIME_WND = 30 #IN DAYS
+TIME_WND = 7 #IN DAYS
 dirpath = os.getcwd()
 
-
+def init_latlon(data):
+    lat_max = data['Latitud'].max()
+    lat_min = data['Latitud'].min()
+    lon_max = data['Longitud'].max()
+    lon_min = data['Longitud'].min()
+    lat_m = data['Latitud'].mean()
+    lon_m = data['Longitud'].mean()
+    
+    return lat_max,lat_min,lon_max,lon_min,lat_m,lon_m
 def build_time_series(data,y_col=0):
     FIRST = data.Date[:1].values[0]
     dim_0 = data.shape[0]- TIME_WND
@@ -24,7 +33,7 @@ def build_time_series(data,y_col=0):
     y = []
     series = []
     X = []
-    k = 1
+    k = 0
     for i in range(dim_0):
         series_df = data[(data.Date >= FIRST+i) & (data.Date <= FIRST+i+TIME_WND)]
         k+=1
@@ -37,22 +46,15 @@ def build_time_series(data,y_col=0):
             #print(FIRST+i+TIME_WND*2)
             y_df = data[(data.Date > FIRST+i+TIME_WND) & (data.Date < FIRST+i+TIME_WND*2)]
             
+            #print('len series',len(series))
             X.append(series)
             y.append(y_df)
             series = []
-            k = 1
+            k = 0
 
     return X,y
 
-def init_latlon(data):
-    lat_max = data['Latitud'].max()
-    lat_min = data['Latitud'].min()
-    lon_max = data['Longitud'].max()
-    lon_min = data['Longitud'].min()
-    lat_m = data['Latitud'].mean()
-    lon_m = data['Longitud'].mean()
-    
-    return lat_max,lat_min,lon_max,lon_min,lat_m,lon_m
+
 def get_marker_color(magnitude):
     # Returns green for small earthquakes, yellow for moderate
     #  earthquakes, and red for significant earthquakes.
@@ -64,7 +66,6 @@ def get_marker_color(magnitude):
         return ('ro')
 
 def create_pre_images_x(X):
-
     
     for i, serie in enumerate(X):
         print(i)
@@ -97,11 +98,11 @@ def create_pre_images_y(y):
    
     for k, data in enumerate(y):
         print(k)
-        series_path =  dirpath+'/images/pre_images/series_'+str(k)+'_'
+        series_path =  dirpath+'/images/pre_output/series_'+str(k)+'_'
         fig = plt.figure(figsize=(8, 8))
         m = Basemap(llcrnrlon=lon_min,llcrnrlat=lat_min,urcrnrlon=lon_max,urcrnrlat=lat_max,
-                    resolution='i',projection='lcc',lat_0=lat_m,lon_0=lon_m)
-        m.shadedrelief()
+            resolution='i',projection='cass',lat_0=lat_m,lon_0=lon_m)
+        #m.shadedrelief()
         m.drawcoastlines(color='gray')
         m.drawcountries(color='gray')
         if not data.empty:
@@ -112,7 +113,7 @@ def create_pre_images_y(y):
                 x,y = m(lon, lat) 
                 marker_string = get_marker_color(mag)
                 m.plot(x, y, marker_string, markersize=10)
-        plt.savefig(series_path+'Yquakemap.jpg')
+        plt.savefig(series_path+'quakemap.jpg')
         plt.close()
 def trim(im):
     bg = Image.new(im.mode, im.size, im.getpixel((0,0)))
@@ -122,28 +123,7 @@ def trim(im):
     if bbox:
         return im.crop(bbox)
 
-dataset = pd.read_csv('data_earthquake_2000-2017.csv',encoding='latin1', sep=';')
-
-
-
-dataset = dataset[dataset['Prof.'] >= 60.0 ]
-
-zone1 = dataset[dataset['Latitud'] > -23]
-zone2 = dataset[(dataset.Latitud < -23) & (dataset.Latitud > -34)] 
-zone3 = dataset[dataset['Latitud'] < -34]
-
-print('Creating Time Series')
-X,y = build_time_series(zone1)
-lat_max,lat_min,lon_max,lon_min,lat_m,lon_m = init_latlon(zone1)
-'''
-print('Creating x photo')
-create_pre_images_x(X)
-print('Creating Y photo')
-create_pre_images_y(y)
-'''
-
-
-def gallery(array, ncols=4):
+def gallery(array, ncols=3):
 
     nindex, height, width, intensity = array.shape
     nrows = nindex//ncols
@@ -167,14 +147,14 @@ def check_shape(array):
            series.append(np.asarray(image)) 
     return series
 
-print('Create gallery...')
+
 def make_array(dim_x):
     
     for i in range(dim_x):
         print('series',i) 
         series = []
         aux = []
-        for j in range(TIME_STEP-1):
+        for j in range(TIME_STEP):
             print('photo',j)
             aux.append(Image.open(dirpath+'/images/pre_images/series_{0}_Xquakemap_{1}.jpg'.format(i,j)).convert('RGB'))
             
@@ -184,12 +164,45 @@ def make_array(dim_x):
         #print(np.shape(series[1]))
        
         im = Image.fromarray(gallery(np.array(series)))
-        im.save(dirpath+'/images/gallery/series_{0}_Xquakemap.jpg'.format(i))
+        im.save(dirpath+'/images/gallery/series_{0}_quakemap.jpg'.format(i))
         im_shape = (im.size)
 
     print(im_shape) 
 
+def triming (namepath):
+    folderName = dirpath+namepath
+    filePaths = glob.glob(folderName + "/*.jpg")
 
+
+    for filePath in filePaths:
+        #print(filePath)
+        image=Image.open(filePath)
+        im = trim(image)
+        im.save(filePath)
+
+dataset = pd.read_csv('data_earthquake_2000-2017.csv',encoding='latin1', sep=';')
+
+dataset = dataset[dataset['Prof.'] >= 60.0 ]
+
+zone1 = dataset[dataset['Latitud'] > -23]
+zone2 = dataset[(dataset.Latitud < -23) & (dataset.Latitud > -34)] 
+zone3 = dataset[dataset['Latitud'] < -34]
+
+print('Creating Time Series')
+X,y = build_time_series(zone1)
+
+lat_max,lat_min,lon_max,lon_min,lat_m,lon_m = init_latlon(zone1)
+
+print('Creating x photo')
+create_pre_images_x(X)
+print('Creating Y photo')
+create_pre_images_y(y)
+
+print('Triming photos')
+triming('/images/pre_images')
+triming('/images/pre_output')
+
+print('Create gallery...')
 
 make_array(len(X))
 
